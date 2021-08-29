0b709cca30c4efbeeff92dc1cd2c781592c7dfc5
João Paulo <joaopdejesus79@gmail.com>
2021-08-28 21:28:58 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Digite seu nome: ");
+            Console.ResetColor();
+            Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Olá ");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(Nome);
+            Console.ResetColor();
+            Console.WriteLine("! Seja bem vindo!");
+            Console.WriteLine();
+        }
+    }
+}
